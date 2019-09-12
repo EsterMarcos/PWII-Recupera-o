d5c58bab0b5c22388fb9b0056ed56de56f1cfe27
Ester <estermarcoshas@gmail.com>
2019-09-11 21:41:04 -0300

    exercicio 2-3 variavel

---
+++ dist/exercicio2-3.js
@@ -0,0 +1,4 @@
+{
+    var salario = 3500.56;
+    console.log("Seu salario é" + salario);
+}
---
+++ exercicio2-3.ts
@@ -0,0 +1,6 @@
+ {
+     var salario :number = 3500.56;
+     
+        console.log ("Seu salario é"+salario);
+
+ }
