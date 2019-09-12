e9bed5494e0929fc68324b8bface7ab134729feb
Ester <estermarcoshas@gmail.com>
2019-09-11 22:14:46 -0300

    Exercicio 6

---
+++ dist/exercicio4-5.js
@@ -0,0 +1,10 @@
+{
+    var num = 3;
+    var mod = num % 2;
+    if (mod = 0) {
+        console.log("O número é par");
+    }
+    else {
+        console.log("O número é impar");
+    }
+}
--- dist/exercicio6.js
+++ dist/exercicio6.js
@@ -1,9 +1,8 @@
 {
-    let largura = 60;
+    var largura = 60;
     console.log("A largura é  " + largura + "  metros");
-    let comprimento =24;
+    var comprimento = 24;
     console.log("O comprimento é  " + comprimento + "  metros");
-    var area =largura*comprimento;
+    var area = largura * comprimento;
     console.log("A area é  " + area + "  metros");
-
 }
